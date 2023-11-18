7fc09fa7aff35d1b76ccd26785e5a5eafdc53ace
jekiquin <jci.dev.0@gmail.com>
2023-11-17 17:40:39 -0800

    added changes

--- src/services/axios.service.tsx
+++ src/services/axios.service.tsx
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseURL = "http://localhost:8888";
+const baseURL = import.meta.env.VITE_BACKEND_SERVER;
 
 //  Public API requests
 export default axios.create({ baseURL });
@@ -8,7 +8,7 @@ export default axios.create({ baseURL });
 //  Private API requests - send authorization header with access token
 export const axiosPrivate = axios.create({
   baseURL,
-  headers: { "Content-Type": "application/json" },
+  headers: { 'Content-Type': 'application/json' },
 });
 
 //  Auth server requests
